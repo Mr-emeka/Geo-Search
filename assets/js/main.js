30e26a8f98f78591afecdba7e68d6393cd7a850c
Mr-emeka <speak2c.emeka@gmail.com>
2019-10-30 00:27:29 +0100

    ft(eslint/airbnb):eslint and airbnb added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,20 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {
+  const uluru = {
     lat: 51.51,
     lng: -0.13,
   };
   // The map, centered at Uluru
-  var map = new google.maps.Map(
+  const map = new google.maps.Map(
     document.getElementById('map'), {
       zoom: 12,
-      center: uluru
-    });
+      center: uluru,
+    },
+  );
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({
+  const marker = new google.maps.Marker({
     position: uluru,
-    map: map
+    map,
   });
 }
